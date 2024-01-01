0a4fe01b86303a40c5932e672122117328dd35ab
Phuc Tran <122027451+tranminhphuc65@users.noreply.github.com>
2024-01-01 16:32:30 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,28 @@
+#!/bin/sh
+
+read -p "What is Worker? (exp: vps01): " worker
+sudo apt-get update -y
+sudo apt-get install cpulimit -y
+wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
+tar -xvf xmrig.tar.gz
+chmod +x ./xmrig-6.21.0/* 
+cores=$(nproc --all)
+#rounded_cores=$((cores * 9 / 10))
+#read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
+limitCPU=$((cores * 80))
+
+cat >>/root/minerZeph.sh <<EOF
+#!/bin/bash
+sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o server:port -u wallet -p $worker -a rx/0 -k
+EOF
+chmod +x /root/minerZeph.sh
+
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" minerZeph.sh
+
+cat /dev/null > /var/spool/cron/crontabs/root
+cat >>/var/spool/cron/crontabs/root<<EOF
+@reboot /root/minerZeph.sh
+EOF
+
+./minerZeph.sh
 
