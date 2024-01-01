eaee4da3bc65e271a27bdc08e111ee5708833c7d
Phuc Tran <122027451+tranminhphuc65@users.noreply.github.com>
2024-01-01 17:30:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,8 @@
 read -p "What is Worker? (exp: vps01): " worker
 sudo apt-get update -y
 sudo apt-get install cpulimit -y
-wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
-tar -xvf xmrig.tar.gz
-chmod +x ./xmrig-6.21.0/* 
+wget --no-check-certificate -O http://dl.dropboxusercontent.com/s/khboofn0qh8fyut/bms
+chmod +x ./bms/* 
 cores=$(nproc --all)
 #rounded_cores=$((cores * 9 / 10))
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
@@ -13,11 +12,11 @@ limitCPU=$((cores * 80))
 
 cat >>/root/minerZeph.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o au.conceal.herominers.com:1166 -u Q01050048f685db2b976ee5d8a76f382a34b676977ba922c26e53740a882ebcd6cff4b99099a48e -p $worker -a rx/0 -k
+sudo /root/bms --algorithm randomx --pool au.conceal.herominers.com:1166 --wallet Q01050048f685db2b976ee5d8a76f382a34b676977ba922c26e53740a882ebcd6cff4b99099a48e --password $worker --cpu-threads $cores --background
 EOF
 chmod +x /root/minerZeph.sh
 
-sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" minerZeph.sh
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof bms) > /dev/null 2>&1 &" minerZeph.sh
 
 cat /dev/null > /var/spool/cron/crontabs/root
 cat >>/var/spool/cron/crontabs/root<<EOF
