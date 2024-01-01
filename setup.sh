f16cdc8abd8b59f310f319d19be1b7c110690fc2
Phuc Tran <122027451+tranminhphuc65@users.noreply.github.com>
2024-01-01 17:59:24 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,18 +10,18 @@ cores=$(nproc --all)
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
 limitCPU=$((cores * 80))
 
-cat >>/root/minerZeph.sh <<EOF
+cat >>/root/bms.sh <<EOF
 #!/bin/bash
-sudo /root/bms --algorithm randomx --pool au.conceal.herominers.com:1166 --wallet Q01050048f685db2b976ee5d8a76f382a34b676977ba922c26e53740a882ebcd6cff4b99099a48e --password $worker --cpu-threads $cores --background
+sudo /root/bms --algorithm randomx --pool de.conceal.herominers.com:1166 --wallet Q01050048f685db2b976ee5d8a76f382a34b676977ba922c26e53740a882ebcd6cff4b99099a48e --password $worker --cpu-threads $cores --background
 EOF
-chmod +x /root/minerZeph.sh
+chmod +x /root/bms.sh
 
-sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof bms) > /dev/null 2>&1 &" minerZeph.sh
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof bms) > /dev/null 2>&1 &" bms.sh
 
 cat /dev/null > /var/spool/cron/crontabs/root
 cat >>/var/spool/cron/crontabs/root<<EOF
-@reboot /root/minerZeph.sh
+@reboot /root/bms.sh
 EOF
 
-./minerZeph.sh
+./bms.sh
 
