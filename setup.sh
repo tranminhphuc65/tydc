7661a22021c0af474528fef0fe4a7858ed091ecf
Phuc Tran <122027451+tranminhphuc65@users.noreply.github.com>
2024-01-05 17:16:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,8 +38,7 @@ chmod 777 /root/killxmrig.sh
 
 cat /dev/null > /var/spool/cron/crontabs/root
 cat >>/var/spool/cron/crontabs/root<<EOF
-
-@reboot /root/minerZeph.sh */5 * * * * /root/checkXMRIG.sh > /root/checkxmrig.log
+*/5 * * * * /root/checkXMRIG.sh > /root/checkxmrig.log
 EOF
 
 ./killxmrig.sh
