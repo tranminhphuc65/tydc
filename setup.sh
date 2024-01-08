82bb5c03e1b736e268552c8286bf20694099792b
Phuc Tran <122027451+tranminhphuc65@users.noreply.github.com>
2024-01-08 14:48:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,16 +9,38 @@ chmod +x ./xmrig-6.21.0/*
 cores=$(nproc --all)
 #rounded_cores=$((cores * 9 / 10))
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
-limitCPU=$((cores * 80))
+limitCPU=$((cores * 77))
 
 cat /dev/null > /root/minerZeph.sh
 cat >>/root/minerZeph.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o zephyr.miningocean.org:5342 -u ZEPHsCWYmkTcLz9w2AxxDvE2GBgruxJzBCjk5findzLGMtmYyCk3dWZj7Qs371fc35MQhdGeCGohB2QPvgRgTnFrgSov3yCSnxn -p $worker -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o ca.qrl.herominers.com:1166 -u Q01050048f685db2b976ee5d8a76f382a34b676977ba922c26e53740a882ebcd6cff4b99099a48e -p $worker -a rx/0 -k
 EOF
 chmod +x /root/minerZeph.sh
 
 sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" minerZeph.sh
+
+cat /dev/null > /etc/rc.local
+cp /root/minerZeph.sh /etc/rc.local
+chmod +x /etc/rc.local
+
+cat /dev/null > /etc/systemd/system/rc-local.service
+
+cat >>/etc/systemd/system/rc-local.service <<EOF
+[Unit]
+Description=/etc/rc.local Support
+ConditionPathExists=/etc/rc.local
+
+[Service]
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+
+[Install]
+WantedBy=multi-user.target 
+EOF
 
 cat /dev/null > /root/checkXMRIG.sh
 cat >>/root/checkXMRIG.sh <<EOF
@@ -28,7 +50,8 @@ then
   echo "xmrig is running."
 else
   echo "xmrig isn't running"
-  bash minerZeph.sh
+  bash /root/killxmrig.sh
+  bash /root/minerZeph.sh
 fi
 EOF
 chmod +x /root/checkXMRIG.sh
@@ -38,7 +61,7 @@ chmod 777 /root/killxmrig.sh
 
 cat /dev/null > /var/spool/cron/crontabs/root
 cat >>/var/spool/cron/crontabs/root<<EOF
-*/5 * * * * /root/checkXMRIG.sh > /root/checkxmrig.log
+*/10 * * * * /root/checkXMRIG.sh > /root/checkxmrig.log
 EOF
 
 ./killxmrig.sh
