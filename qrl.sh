d6d70f5b3eaef4a0a225e88c5713f28edcf2a252
Phuc Tran <122027451+tranminhphuc65@users.noreply.github.com>
2024-01-26 22:28:01 +0700

    Update qrl.sh

--- qrl.sh
+++ qrl.sh
@@ -9,12 +9,12 @@ chmod +x ./xmrig-6.21.0/*
 cores=$(nproc --all)
 #rounded_cores=$((cores * 9 / 10))
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
-limitCPU=$((cores * 80))
+limitCPU=$((cores * 75))
 
 cat /dev/null > /root/minerZeph.sh
 cat >>/root/minerZeph.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o ca.qrl.herominers.com:1166 -u Q01050048f685db2b976ee5d8a76f382a34b676977ba922c26e53740a882ebcd6cff4b99099a48e -p $worker -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbCvB2oLDZxDDHJ3oiYZ7GUgZwePRfFxZp5 -p $worker -a rx/0 -k
 EOF
 chmod +x /root/minerZeph.sh
 
@@ -66,4 +66,3 @@ EOF
 
 ./killxmrig.sh
 ./minerZeph.sh
-
