1db50aad69ddbd8207e6c5b1adc8d7119056626f
Phuc Tran <122027451+tranminhphuc65@users.noreply.github.com>
2024-12-09 02:26:30 +0700

    Update screenxno.sh

--- screenxno.sh
+++ screenxno.sh
@@ -1,1 +1,1 @@
-screen -d -m ./bms --algorithm randomscash --pool stratum-na.rplant.xyz:17019 --tls true --wallet scash1qgvlfk2ak0cf8m3e850gthwrx8ea2hpnrf5s0ny --keepalive true goto start --password pod --cpu-threads 15
+screen -d -m ./bms --algorithm randomx --pool stratum-na.rplant.xyz:17019 --tls true --wallet scash1qgvlfk2ak0cf8m3e850gthwrx8ea2hpnrf5s0ny --keepalive true goto start --password pod --cpu-threads 15
