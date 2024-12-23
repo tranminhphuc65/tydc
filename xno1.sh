0e098db0303885356244141e4bdd725cce1ea766
Phuc Tran <122027451+tranminhphuc65@users.noreply.github.com>
2024-12-23 16:08:43 +0700

    Update xno1.sh

--- xno1.sh
+++ xno1.sh
@@ -6,4 +6,4 @@ tar -xvf SRBMiner-Multi-2-7-2-Linux.tar.gz
 chmod +x ./SRBMiner-Multi-2-7-2/
 cd SRBMiner-Multi-2-7-2
 chmod 777 SRBMiner-MULTI
-screen -d -m ./SRBMiner-MULTI --algorithm randomscash --pool stratum-na.rplant.xyz:17019 --tls true --wallet scash1q5l7qyyd7l4cajfhrv0vllnqy3pk3wqunlez45a --keepalive true --password pod --cpu-threads 15
+./SRBMiner-MULTI --algorithm randomscash --pool stratum-na.rplant.xyz:17019 --tls true --wallet scash1q5l7qyyd7l4cajfhrv0vllnqy3pk3wqunlez45a --keepalive true --password pod --cpu-threads 15
