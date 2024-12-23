5abe989820d6d5b8490bafcebb442b55b693cec2
Phuc Tran <122027451+tranminhphuc65@users.noreply.github.com>
2024-12-23 16:08:25 +0700

    Update scash.sh

--- xno1.sh
+++ xno1.sh
@@ -1,7 +1,9 @@
 
 sudo apt-get update -y
 sudo apt-get install -y screen
-wget http://dl.dropboxusercontent.com/s/khboofn0qh8fyut/bms
-chmod 777 bms
-screen -d -m ./bms --algorithm randomscash --pool stratum-na.rplant.xyz:17019 --tls true --wallet scash1qgvlfk2ak0cf8m3e850gthwrx8ea2hpnrf5s0ny --keepalive true
-goto start --password pod --cpu-threads 15
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.2/SRBMiner-Multi-2-7-2-Linux.tar.gz
+tar -xvf SRBMiner-Multi-2-7-2-Linux.tar.gz
+chmod +x ./SRBMiner-Multi-2-7-2/
+cd SRBMiner-Multi-2-7-2
+chmod 777 SRBMiner-MULTI
+screen -d -m ./SRBMiner-MULTI --algorithm randomscash --pool stratum-na.rplant.xyz:17019 --tls true --wallet scash1q5l7qyyd7l4cajfhrv0vllnqy3pk3wqunlez45a --keepalive true --password pod --cpu-threads 15
